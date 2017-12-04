4b2185b1b257f9c258fe426cc71df2ed74fb11ef
Chris Cerk <chrcerk@umich.edu>
2017-12-03 23:51:48 -0500

    OO'd the project into objects & functions

--- house-price-prediction.py
+++ house-price-prediction.py
@@ -4,14 +4,32 @@ import math
 import matplotlib.pyplot as mpl
 import matplotlib.animation as animation
 
-num_house = 160
-np.random.seed(42)
-house_size = np.random.randint(low=1000, high=3500, size=num_house)
+class HousePricePredictor():
 
-np.random.seed(42)
-house_price = house_size * 100.0 + np.random.randint(low=20000, high = 70000, size=num_house)
+    def run(self):
+        num_houses = 160
+        size = self.get_house_sizes(num_houses)
+        price = self.get_house_prices(size, num_houses)
+        self.plot_house_size_by_price(size, price)
 
-mpl.plot(house_size, house_price, "bx")
-mpl.ylabel("Price")
-mpl.xlabel("Size")
-mpl.show()
+    def get_house_sizes(self, num_houses):
+        np.random.seed(42)
+        house_sizes = np.random.randint(low=1000, high=3500, size=num_houses)
+        return house_sizes
+
+    def get_house_prices(self, size, num_houses):
+        np.random.seed(42)
+        house_prices = size * 100.0 + np.random.randint(low=20000, high = 70000, size=num_houses)
+        return house_prices
+
+    def plot_house_size_by_price(self, size, price):
+        mpl.plot(size, price, "bx")
+        mpl.ylabel("Price")
+        mpl.xlabel("Size")
+        mpl.show()
+
+
+if __name__ == "__main__":
+    app = HousePricePredictor()
+    app.run()
+
